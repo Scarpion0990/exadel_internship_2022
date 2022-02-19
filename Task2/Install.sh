82095fd696f7123094560ba311f110a6598ef349
scarpion0990 <scarpion0990@mail.ru>
2022-02-19 22:20:49 +0500

    AWS clouds

--- Task2/Install.sh
+++ Task2/Install.sh
@@ -2,6 +2,9 @@
 yum -y update
 yum -y install httpd
 OS_VERSION=$(cat /proc/version)
-echo "<html><body><center><h1><p>Hello World <br> $OS_VERSION</p></h1></center></body></html>" > /var/www/html/index.html
+DISK_FREE=$(df -h)
+DISK_USAGE=$(du -h)
+ALL_PROCCESSES=$(ps -aux)
+echo "<html><body><center><h1><p>Hello World <br> $OS_VERSION</p> <br> <pre> $DISK_FREE </pre> <br> <pre> $DISK_USAGE </pre> <br> <pre> $ALL_PROCCESSES </pre></h1></center></body></html>" > /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
